77915f302743ce9a13c0449fcd38f27ec29d6865
baby4dm <babych.dmtr@gmail.com>
2025-12-02 20:59:02 +0200

    created basic structure

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: "/space-tourism-multi-website/",
-
   plugins: [tailwindcss()],
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/pages/index.html"),
+        crew: resolve(__dirname, "src/pages/crew.html"),
+        destination: resolve(__dirname, "src/pages/destination.html"),
+        technology: resolve(__dirname, "src/pages/technology.html"),
+      },
+    },
+  },
 });
