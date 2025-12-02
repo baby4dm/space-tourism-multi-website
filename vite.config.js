aa6ac79e1027099255234430cdc363b27f1ed75f
baby4dm <babych.dmtr@gmail.com>
2025-12-02 22:39:50 +0200

    created home page

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "path";
+import htmlInject from "vite-plugin-html-inject";
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [tailwindcss(), htmlInject()],
 
   build: {
     rollupOptions: {
