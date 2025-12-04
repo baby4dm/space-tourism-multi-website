ecd4a0131f2fe7de285acb78662f2694cd10e566
baby4dm <babych.dmtr@gmail.com>
2025-12-04 23:00:21 +0200

    added mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const currentPage = window.location.pathname.split("/").pop();
-  const navItems = document.querySelectorAll(".nav-item");
+
+  const navItems = document.querySelectorAll(".nav-item, .mobile-nav-item");
 
   navItems.forEach((item) => {
     const href = item.getAttribute("href");
@@ -12,5 +13,18 @@ document.addEventListener("DOMContentLoaded", () => {
     if (currentPage === "" && href === "index.html") {
       item.classList.add("active");
     }
+  });
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger-btn");
+  const closeBtn = document.querySelector(".close-btn");
+  const mobileMenu = document.querySelector(".mobile-menu");
+
+  hamburger.addEventListener("click", () => {
+    mobileMenu.classList.add("mobile-menu-open");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    mobileMenu.classList.remove("mobile-menu-open");
   });
 });
