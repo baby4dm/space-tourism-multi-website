f430b38d286ad44903917f54dd011637386de0e4
baby4dm <babych.dmtr@gmail.com>
2025-12-06 01:05:26 +0200

    added deployment config

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,22 @@ import htmlInject from "vite-plugin-html-inject";
 
 export default defineConfig({
   base: "/space-tourism-multi-website/",
+
+  // Головна папка тепер src/pages.
+  // Vite навіть не подивиться на твій index.html з редіректом у корені проекту.
+  root: resolve(__dirname, "src/pages"),
+
+  publicDir: resolve(__dirname, "public"),
+
   plugins: [tailwindcss(), htmlInject()],
 
   build: {
+    outDir: resolve(__dirname, "dist"),
+    emptyOutDir: true,
+
     rollupOptions: {
       input: {
+        // Абсолютні шляхи - це найнадійніший варіант
         main: resolve(__dirname, "src/pages/index.html"),
         crew: resolve(__dirname, "src/pages/crew.html"),
         destination: resolve(__dirname, "src/pages/destination.html"),
