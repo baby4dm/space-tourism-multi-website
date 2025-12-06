d5cfbc840bd695cd51603dedd8328ab17e0056e2
baby4dm <babych.dmtr@gmail.com>
2025-12-06 15:09:46 +0200

    fixed path

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import htmlInject from "vite-plugin-html-inject";
 
 export default defineConfig({
   base: "/space-tourism-multi-website/",
 
-  plugins: [tailwindcss()],
+  plugins: [
+    tailwindcss(),
+    htmlInject({
+      injectData: {
+        header: "/src/components/Header.html",
+      },
+    }),
+  ],
 
   build: {
     rollupOptions: {
